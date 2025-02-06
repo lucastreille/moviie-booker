02e5d09703d2692104a2052b6b6e53159c0c8fe0
lucastreille <lucas.treille1.0@gmail.com>
2025-02-06 16:51:15 +0100

    [FIX] update port

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const config = new DocumentBuilder()
     .setTitle('API Documentation')
     .setDescription('Description de l\'API') 
@@ -16,13 +16,16 @@ async function bootstrap() {
   SwaggerModule.setup('api-docs', app, document);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
-    credentials: true,
+    origin: '*',  
+    credentials: true,  
   });
   
+
   app.setGlobalPrefix('api');
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
